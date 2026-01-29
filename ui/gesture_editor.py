2fac8dd5649ea97f9e7fdc4c7447d97650b5aa46
Isaac Cohen <isaaccohen2007@icloud.com>
2026-01-29 12:21:23 +0200

    Remove legacy PySide UI

--- ui/gesture_editor.py
+++
@@ -1,8 +0,0 @@
-"""UI placeholder for gesture editor."""
-
-from utils.log_utils import tprint
-
-
-class GestureEditor:
-    def open(self) -> None:
-        tprint("[UI] Gesture editor opened")
--- ui/voice_editor.py
+++
@@ -1,8 +0,0 @@
-"""UI placeholder for voice command editor."""
-
-from utils.log_utils import tprint
-
-
-class VoiceEditor:
-    def open(self) -> None:
-        tprint("[UI] Voice editor opened")
